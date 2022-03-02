0a37fe1ba44f4bb432220ea1a01059564a697a4c
ExoticDuck <marchenkov.n@inbox.ru>
2022-03-02 19:14:47 +0300

    Updates

---
+++ precache-manifest.af7e35a16749e930fdaa247801ac2b1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "368ff640ed94371a7361defb73efc4ad",
+    "url": "/Todolist/index.html"
+  },
+  {
+    "revision": "6ccfe89f0a3b7cf5ff61",
+    "url": "/Todolist/static/css/main.6f30ddfc.chunk.css"
+  },
+  {
+    "revision": "600ca4e835019e1a1fe8",
+    "url": "/Todolist/static/js/2.07250795.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Todolist/static/js/2.07250795.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ccfe89f0a3b7cf5ff61",
+    "url": "/Todolist/static/js/main.c2d97496.chunk.js"
+  },
+  {
+    "revision": "8c9ba947015c2dd63600",
+    "url": "/Todolist/static/js/runtime-main.e8babc40.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ExoticDuck/Todolist/precache-manifest.4887d7234359d8f0a00dba227287dc42.js"
+  "/Todolist/precache-manifest.af7e35a16749e930fdaa247801ac2b1f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ExoticDuck/Todolist/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Todolist/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
